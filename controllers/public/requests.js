2a489c6c2c05a3ccd06164248806a5949a8fe15e
ryukomvp <daniel123hernandez15@gmail.com>
2024-06-12 01:15:38 -0600

    leer razas disponibles en ventana para registrar un gato (sitio publico)

---
+++ controllers/public/requests.js
@@ -0,0 +1,8 @@
+// Constante para completar la ruta de la API.
+const RAZAS_API = 'business/dashboard/breeds.php';
+
+// Método manejador de eventos para cuando el documento ha cargado.
+document.addEventListener('DOMContentLoaded', () => {
+    // Llamada a la función para llenar la tabla con los registros disponibles.
+    fillSelect(RAZAS_API, 'readAll', 'raza');
+});
